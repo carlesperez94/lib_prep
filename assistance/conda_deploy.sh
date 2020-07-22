4c2b05f05d76b555ba425250a0835753e4c84057
Carles Perez Lopez <carlesperez94@gmail.com>
2020-07-22 10:40:27 +0200

    Conda deployment assistance

---
+++ assistance/conda_deploy.sh
@@ -0,0 +1,4 @@
+cd ../conda_recipe
+conda-build -c conda-forge -c nostrumbiodiscovery -c rdkit -c carlesperez94 --python=3.6 .
+conda-build -c conda-forge -c nostrumbiodiscovery -c rdkit -c carlesperez94 --python=3.7 .
+conda-build -c conda-forge -c nostrumbiodiscovery -c rdkit -c carlesperez94 --python=3.8 .
